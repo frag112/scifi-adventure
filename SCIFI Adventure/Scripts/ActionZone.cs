cecf5fcd860fdfa3251dbe2a57253df2849e3d8e
frag112 <lumchtime@gmail.com>
2023-01-22 11:49:44 +0300

    interactible script template + door script + door animation + sounds

---
+++ SCIFI Adventure/Scripts/ActionZone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScifiAdventure
+{
+    [RequireComponent(typeof(Collider))]
+    public class ActionZone : MonoBehaviour
+    {
+        [SerializeField] private bool _buttonPressed;
+        [SerializeField] private Interactible _interactible;
+        
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player") && _buttonPressed)
+            {
+                _interactible.Interract();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _interactible.Leave();
+            }
+        }
+    }
+}
+
